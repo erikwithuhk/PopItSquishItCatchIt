609dc0165adc0ca322e111ddf63d65ca2982227d
Erik Jönsson <efjonsson@gmail.com>
2016-08-31 11:47:58 -0400

    gameTargetView#new creates a div node

--- scripts/gameTargetView.js
+++ scripts/gameTargetView.js
@@ -1,1 +1,8 @@
-console.log('gameTargetView');
+class gameTargetView {
+  constructor() {
+    this.el = document.createElement('div');
+  }
+}
+
+const test = new gameTargetView();
+console.log(test.el);
