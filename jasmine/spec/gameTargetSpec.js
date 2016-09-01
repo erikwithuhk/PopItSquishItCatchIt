e1f47200fa1b06405071be7a6ecaeb7108bfb4df
Erik Jönsson <efjonsson@gmail.com>
2016-09-01 12:24:25 -0400

    remove GameTarget#generatePointValue and initalize point value at 0

--- jasmine/spec/gameTargetSpec.js
+++ jasmine/spec/gameTargetSpec.js
@@ -9,26 +9,8 @@ describe('GameTarget', () => {
 
   describe('#new', () => {
 
-    it('Sets a point value property', () => {
-      expect(gameTarget.pointValue).toBeTruthy();
-    });
-
-  });
-
-
-  describe('#generatePointValue', () => {
-
-    it('Generates a random point value between 1 and 10', () => {
-      const randomValue1 = gameTarget.generatePointValue();
-      const randomValue2 = gameTarget.generatePointValue();
-      const randomValue3 = gameTarget.generatePointValue();
-
-      expect(randomValue1).toBeGreaterThan(0);
-      expect(randomValue1).toBeLessThan(11);
-      expect(randomValue2).toBeGreaterThan(0);
-      expect(randomValue2).toBeLessThan(11);
-      expect(randomValue3).toBeGreaterThan(0);
-      expect(randomValue3).toBeLessThan(11);
+    it('Sets a point value property to 0', () => {
+      expect(gameTarget.pointValue).toEqual(0);
     });
 
   });
--- scripts/gameTarget.js
+++ scripts/gameTarget.js
@@ -1,10 +1,6 @@
 class GameTarget {
   constructor() {
-    this.pointValue = this.generatePointValue();
-  }
-  generatePointValue() {
-    const randomValue = Math.floor(Math.random() * 9) + 1;
-    return randomValue;
+    this.pointValue = 0;
   }
   setPointValue(targetType, targetValue) {
     const pointValues = {
