6350bafbcd7e838e1b41deb66049e08a0b80d1c8
Erik Jönsson <efjonsson@gmail.com>
2016-09-01 16:24:21 -0400

    count down from 30 seconds

--- scripts/round.js
+++ scripts/round.js
@@ -2,16 +2,29 @@ class Round {
   constructor() {
     this.score = 0;
     this.gameTargets = [];
+    this.timeRemaining = 30;
+    this.startCountdownTimer();
   }
   addGameTarget(gameTarget) {
     this.gameTargets.push(gameTarget);
   }
+  startCountdownTimer() {
+    window.setInterval(() => {
+      if (this.timeRemaining > 0) {
+        this.timeRemaining -= 1;
+      } else {
+        this.timeRemaining = 0;
+      }
+      console.log(this.timeRemaining);
+    }, 1000);
+  }
   getTotalScore() {
-    let pointsFromAllGameTargets = this.gameTargets.map((gameTarget) => {
+    const pointsFromAllGameTargets = this.gameTargets.map((gameTarget) => {
       return gameTarget.getPointValue();
     });
     const totalPoints = pointsFromAllGameTargets.reduce((curr, prev) => {
       return curr + prev;
     });
+    return totalPoints;
   }
 }
