ea545be4b2c354c4b5b744b6d0caae5f076a8c7a
Erik Jönsson <efjonsson@gmail.com>
2016-09-01 12:30:40 -0400

    create roundView and roundViewSpec

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,7 @@
-function kill(node) {
-  window.setInterval(() => {
-    document.body.removeChild(node)
-  }, 30000);
-}
-
 let id = 1;
 window.setInterval(() => {
   const gameTargetViewTest = new GameTargetView(id);
   gameTargetViewTest.render();
   document.body.appendChild(gameTargetViewTest.el)
   id += 1;
-  kill(gameTargetViewTest.el);
 }, 500);
