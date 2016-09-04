e3c7a6636eac053ed1ed88183ea2187bfc762a4b
Erik Jönsson <efjonsson@gmail.com>
2016-09-03 21:24:53 -0400

    resize current score font on window resize

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
-// Resizes font-size based on viewport
+// Resizes font-size based when viewport is resized
 window.addEventListener('resize', () => {
   document.querySelector('.timer').style.zIndex = '0';
+  document.querySelector('.current-score').style.zIndex = '0';
 });
 
 const game = new Game(30);
